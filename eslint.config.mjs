97a6b303a7d29cccf0d072b2b43bc6c7055b310c
Bhaska <muhammadbhaska0@gmail.com>
2025-11-16 16:43:47 +0700

    feat: update nextjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,10 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default defineConfig([
+  ...nextCoreWebVitals,
+  ...nextTypescript,
   {
     languageOptions: {
       parserOptions: {
@@ -24,6 +18,4 @@ const eslintConfig = [
       "jsx-a11y/alt-text": "off",
     },
   },
-];
-
-export default eslintConfig;
+]);
